dce63e047be9939ea7539ce3063afc57a7e66e70
Maciej Witowski <witowski.maciej@gmail.com>
2025-07-17 14:12:40 +0200

    Fix

--- app/api/operations.py
+++ app/api/operations.py
@@ -12,7 +12,7 @@ async def create_operation(request: CreateOperationRequest) -> CreateOperationRe
     try:
         prediction: ReplicatePredictionResponse = replicate_provider.create_prediction(
             signature=request.app_signature,
-            operation=request.operation_request_json,
+            operation_request_json=request.operation_request_json,
         )
 
         response = CreateOperationResponse(
