603885a95a8c48e15963f2f8f52f9470333441a6
Maciej Witowski <witowski.maciej@gmail.com>
2025-07-18 16:24:14 +0200

    Remove app directory and add proper Cloud Build configuration for root structure

--- app/grants/__init__.py
+++
@@ -1,9 +0,0 @@
-# Grants module for data portability personal server
-
-from .fetch import fetch_raw_grant_file
-from .validate import validate
-
-__all__ = [
-    "fetch_raw_grant_file",
-    "validate",
-]
--- app/utils/files/__init__.py
+++
@@ -1,9 +0,0 @@
-from .download import download_file
-from .decrypt import decrypt, decrypt_with_private_key, decrypt_user_data
-
-__all__ = [
-    "download_file",
-    "decrypt",
-    "decrypt_with_private_key",
-    "decrypt_user_data",
-]
